bab2f03a1e35a2cac790a9578c0310f72e6d616a
raghu1047 <raghuteja.my07@outlook.com>
2022-04-15 12:19:38 +0530

    Initial Commit For Petclinic

---
+++ spring-pet-clinic/spring-pet-web/src/test/java/com/raghu/spring/springpetclinic/web/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghu.spring.springpetclinic.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
